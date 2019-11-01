4c2e322fea858149ab6c251c7e7bb691e1119b5e
Marc <mdreem@fastmail.fm>
2019-11-01 23:23:46 +0100

    copy libraries from installation

--- build.sh
+++ build.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 ROOT=$(pwd)
 
+MONOGAME=/Library/Frameworks/Mono.framework/External/xbuild/MonoGame/v3.0/
+
+cp ${MONOGAME}/Assemblies/DesktopGL/libSDL2-2.0.0.dylib ${ROOT}/Dreetris
+cp ${MONOGAME}/Assemblies/DesktopGL/libopenal.1.dylib ${ROOT}/Dreetris
+cp ${MONOGAME}/Assemblies/DesktopGL/MonoGame.Framework.dll.config ${ROOT}/Dreetris
+cp -r ${MONOGAME}/Assemblies/DesktopGL/x64 ${ROOT}/Dreetris
+cp -r ${MONOGAME}/Assemblies/DesktopGL/x86 ${ROOT}/Dreetris
+
+chmod 644 ${ROOT}/Dreetris/libSDL2-2.0.0.dylib
+chmod 644 ${ROOT}/Dreetris/libopenal.1.dylib
+chmod 644 ${ROOT}/Dreetris/MonoGame.Framework.dll.config
+chmod 644 ${ROOT}/Dreetris/x64/*
+chmod 644 ${ROOT}/Dreetris/x86/*
+
 msbuild ./Dreetris/Dreetris.csproj /p:Configuration=Release /v:d /t:Build
 
 MGCB_DIR=/Library/Frameworks/Mono.framework/External/xbuild/MonoGame/v3.0/Tools
